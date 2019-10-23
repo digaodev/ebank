b556b86c470220b9caa26c0ed9f5ea33bb1c82bb
digaodev <rodrigo.rb.dev@gmail.com>
2019-10-23 19:07:39 -0300

    feat: - add dashboard component and its styles; - add format currency util.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,152 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-date-picker';
 
-import { Container } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+import Header from '../../components/Header';
+import Statement from '../../components/Statement';
+
+import {
+  Container,
+  Content,
+  Aside,
+  SearchCard,
+  SearchButton,
+  SearchIcon,
+  UpArrowIcon,
+  DownArrowIcon,
+  BalanceLoading,
+} from './styles';
+
+const fakeBalance = {
+  balance: 30578,
+  blockedBalance: 0,
+};
+
+const fakeStatements = [
+  {
+    id: Math.random(),
+    amount: 1234,
+    balance: 1234,
+    createdAt: '2019-11-18T16:07:42.000Z',
+    operationType: 'RECEIVED_TRANSFERENCE',
+    otherInfo: {
+      senderAccount: '00714671000114',
+      description: 'Money from dinner last week',
+    },
+  },
+  {
+    id: Math.random(),
+    amount: 200,
+    balance: 200,
+    createdAt: '2019-11-18T16:07:42.000Z',
+    operationType: 'RECEIVED_TRANSFERENCE',
+    otherInfo: {
+      senderAccount: '00714671000114',
+      description: 'Money from delicious candy',
+    },
+  },
+];
 
 export default function Dashboard() {
-  return <div />;
+  const [balance, setBalance] = useState(null);
+  const [statements, setStatements] = useState(null);
+  const [fromDate, setFromDate] = useState();
+  const [toDate, setToDate] = useState();
+
+  async function searchStatementsByDate(ev) {
+    ev.preventDefault();
+
+    // const { data } = await api.get('/b2b/statement');
+
+    setStatements(fakeStatements);
+  }
+
+  function onChangeFromDate(date) {
+    setFromDate(date);
+  }
+
+  function onChangeToDate(date) {
+    setToDate(date);
+  }
+
+  function renderBalance() {
+    if (!balance) {
+      return <BalanceLoading />;
+    }
+
+    if (balance > 0) {
+      return (
+        <>
+          <p>{formatPrice(balance)}</p>
+          <UpArrowIcon size={32} />
+        </>
+      );
+    }
+
+    return (
+      <>
+        <p>{formatPrice(balance)}</p>
+        <DownArrowIcon size={32} />
+      </>
+    );
+  }
+
+  useEffect(() => {
+    async function loadBalance() {
+      // const { data } = await api.get('/account/balance');
+
+      setBalance(fakeBalance.balance);
+
+      setStatements(fakeStatements);
+    }
+
+    loadBalance();
+  }, []);
+
+  return (
+    <Container>
+      <Header />
+
+      <Aside>
+        <aside>
+          <h3>SALDO</h3>
+
+          <span>{renderBalance()}</span>
+        </aside>
+      </Aside>
+
+      <Content>
+        <SearchCard>
+          <form>
+            <p>Listar extrato de</p>
+
+            <DatePicker
+              onChange={onChangeFromDate}
+              value={fromDate}
+              format="dd/MM/yyyy"
+              locale="pt-br"
+            />
+
+            <p>a</p>
+            <DatePicker
+              onChange={onChangeToDate}
+              value={toDate}
+              format="dd/MM/yyyy"
+              locale="pt-br"
+            />
+
+            <SearchButton type="submit" onClick={searchStatementsByDate}>
+              <SearchIcon size="20" />
+              <p>Pesquisar</p>
+            </SearchButton>
+          </form>
+        </SearchCard>
+
+        <p>{statements && `${statements.length} transações encontradas`}</p>
+        {statements &&
+          statements.map(stmt => <Statement key={stmt.id} data={stmt} />)}
+      </Content>
+    </Container>
+  );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,3 +1,148 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { MdSearch, MdArrowUpward, MdArrowDownward } from 'react-icons/md';
 
-export const Container = styled.div``;
+export const SearchIcon = styled(MdSearch)`
+  color: ${props => props.theme.colors.white};
+`;
+export const UpArrowIcon = styled(MdArrowUpward)`
+  color: ${props => props.theme.colors.success};
+`;
+export const DownArrowIcon = styled(MdArrowDownward)`
+  color: ${props => props.theme.colors.error};
+`;
+
+export const Container = styled.div`
+  display: flex;
+
+  background: ${props => props.theme.colors.background};
+  min-height: 100vh;
+`;
+
+const balanceloadingAnimation = keyframes`
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 16px;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 20px;
+  }
+`;
+
+export const BalanceLoading = styled.div`
+  &,
+  &:before,
+  &:after {
+    background: ${props => props.theme.colors.primary};
+    animation: ${balanceloadingAnimation} 1s infinite ease-in-out;
+    width: 12px;
+    height: 12px;
+  }
+  & {
+    color: ${props => props.theme.colors.secondary};
+    text-indent: -9999em;
+    margin: 20px auto;
+    position: relative;
+    font-size: 11px;
+    transform: translateZ(0);
+    animation-delay: -0.16s;
+  }
+  &:before,
+  &:after {
+    position: absolute;
+    top: 0;
+    content: '';
+  }
+  &:before {
+    left: -1.5em;
+    animation-delay: -0.32s;
+  }
+  &:after {
+    left: 1.5em;
+  }
+`;
+
+export const Aside = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  background: ${props => props.theme.colors.background};
+  margin: 100px 24px 0;
+
+  aside {
+    position: fixed;
+
+    h3 {
+      color: ${props => props.theme.colors.darkgray};
+    }
+
+    span {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      p {
+        font-size: ${props => props.theme.fontSizes[4]};
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 100px auto;
+  max-width: 800px;
+
+  > p {
+    margin-bottom: ${props => props.theme.space[2]};
+  }
+`;
+
+export const SearchCard = styled.section`
+  background: ${props => props.theme.colors.white};
+  border-radius: ${props => props.theme.space[1]};
+  box-shadow: ${props => props.theme.shadows.card};
+  margin-bottom: ${props => props.theme.space[5]};
+  padding: ${props => props.theme.space[4]};
+
+  form {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    > p {
+      color: ${props => props.theme.colors.text};
+      font-size: ${props => props.theme.fontSizes[2]};
+    }
+
+    .react-date-picker {
+      padding: 0 ${props => props.theme.space[3]};
+    }
+  }
+`;
+export const SearchButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background: ${props => props.theme.colors.primary};
+  border-radius: 3px;
+  padding: 4px 8px;
+
+  transition: all 0.3s;
+
+  &:hover {
+    background: ${props => props.theme.colors.cyan};
+    box-shadow: ${props => props.theme.shadows.sm};
+    transform: translateY(-1px);
+  }
+
+  > p {
+    color: ${props => props.theme.colors.white};
+    padding: 0 12px;
+  }
+`;
